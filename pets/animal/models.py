086cbafbe659b75a473deab3c11d329fe16a1441
jsleetw <ail529@gmail.com>
2013-02-03 05:35:07 +0800

    add animal model

--- pets/animal/models.py
+++ pets/animal/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-
+import datetime
 
 class Animal(models.Model):
-    name = models.CharField(max_length=200)
     accept_num = models.IntegerField()
-    pub_date = models.DateTimeField('date published')
+    name = models.CharField(max_length=200)
+    sex = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    build = models.CharField(max_length=200)
+    age = models.CharField(max_length=200)
+    variety = models.CharField(max_length=200)
+    reason = models.CharField(max_length=200)
+    accept_num = models.CharField(max_length=200)
+    chip_num = models.CharField(max_length=200)
+    is_sterilization = models.CharField(max_length=200)
+    hair_type = models.CharField(max_length=200)
+    note = models.TextField()
+    resettlement = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    email = models.EmailField()
+    childre_anlong = models.CharField(max_length=200)
+    animal_anlong = models.CharField(max_length=200)
+    bodyweight = models.CharField(max_length=200)
+    image_name = models.URLField(max_length=200)
+    pub_date = models.DateTimeField(auto_now=True)
