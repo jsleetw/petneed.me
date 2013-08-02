0215a06ab6c9cba8b6cb46e21a765c0c41db7f34
jsleetw <ail529@gmail.com>
2013-08-02 18:34:18 +0800

    use pets.db

--- pets/pets/settings.py
+++ pets/pets/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/ail/mydev/pets/pets/pets.db', # Or path to database file if using sqlite3.
+        'NAME': 'pets.db', # Or path to database file if using sqlite3.
         #'USER': '',                      # Not used with sqlite3.
         #'PASSWORD': '',                  # Not used with sqlite3.
         #'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
