9b8364704a81b953de77fdbc87d9102a25a6b60f
jsleetw <ail529@gmail.com>
2013-08-28 17:21:44 +0800

    fix some view code for pep8

--- pets/animal/views.py
+++ pets/animal/views.py
@@ -5,13 +5,12 @@ import urllib2
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from models import Animal
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator
 from django.template import RequestContext
 from django.utils import simplejson
-from urllib2 import HTTPError
-from django.http import HttpResponse
 from django.conf import settings
 from animal.models import User
+
 
 def home(request):
     animals = Animal.objects.order_by("-id")
@@ -22,8 +21,8 @@ def home(request):
     for i in animals:
         i.smal_img_file = "%s_248x350.jpg" % i.image_file.split(".jpg")[0]
 
-    return render_to_response('index.html', {"animals": animals},
-            context_instance=RequestContext(request) )
+    return render_to_response('index.html', {"animals": animals}, context_instance=RequestContext(request))
+
 
 def page(request):
     page = int(request.path_info.strip('/animal/page/'))
@@ -33,11 +32,12 @@ def page(request):
     print animals
     for i in animals:
         i.smal_img_file = "%s_248x350.jpg" % i.image_file.split(".jpg")[0]
-    return render_to_response('page.html', {"animals": animals} )
+    return render_to_response('page.html', {"animals": animals})
+
 
 def login(request):
-      return render_to_response('login.html',
-              context_instance=RequestContext(request))
+    return render_to_response('login.html', context_instance=RequestContext(request))
+
 
 def get_animals(request):
     animals = Animal.objects.order_by('-id')
@@ -49,60 +49,61 @@ def get_animals(request):
             animals = pages.page(page_num)
         else:
             animals = pages.page(1)
-    json = simplejson.dumps([{'accept_num': animal.accept_num,
-                                    'name': animal.name,
-                                    'sex': animal.sex,
-                                    'type': animal.type,
-                                    'build': animal.build,
-                                    'age': animal.age,
-                                    'variety': animal.variety,
-                                    'reason': animal.reason,
-                                    'chip_num': animal.chip_num,
-                                    'is_sterilization': animal.is_sterilization,
-                                    'hair_type': animal.hair_type,
-                                    'note': animal.note.replace('"', '\\"'),
-                                    'resettlement': animal.resettlement,
-                                    'phone': animal.phone,
-                                    'email': animal.email,
-                                    'childre_anlong': animal.childre_anlong,
-                                    'animal_anlong': animal.animal_anlong,
-                                    'bodyweight': animal.bodyweight,
-                                    'image_name': animal.image_name,
-                                    'image_file': animal.image_file,
-                                    'pub_date': animal.pub_date.strftime('%B %d, %Y') } 
-                                        for animal in animals] )
+    api_json = simplejson.dumps([{'accept_num': animal.accept_num,
+                                  'name': animal.name,
+                                  'sex': animal.sex,
+                                  'type': animal.type,
+                                  'build': animal.build,
+                                  'age': animal.age,
+                                  'variety': animal.variety,
+                                  'reason': animal.reason,
+                                  'chip_num': animal.chip_num,
+                                  'is_sterilization': animal.is_sterilization,
+                                  'hair_type': animal.hair_type,
+                                  'note': animal.note.replace('"', '\\"'),
+                                  'resettlement': animal.resettlement,
+                                  'phone': animal.phone,
+                                  'email': animal.email,
+                                  'childre_anlong': animal.childre_anlong,
+                                  'animal_anlong': animal.animal_anlong,
+                                  'bodyweight': animal.bodyweight,
+                                  'image_name': animal.image_name,
+                                  'image_file': animal.image_file,
+                                  'pub_date': animal.pub_date.strftime('%B %d, %Y')} for animal in animals])
     # print json.decode("unicode_escape")
-    return HttpResponse(json.decode('unicode_escape'), mimetype='application/json')
+    return HttpResponse(api_json.decode('unicode_escape'), mimetype='application/json')
+
 
 def get_specific_animal(request, accept_num):
-    animal = Animal.objects.filter(accept_num = accept_num)
+    animal = Animal.objects.filter(accept_num=accept_num)
     print animal
     if len(animal) > 0:
         animal = animal[0]
     else:
         return HttpResponse(status=404, mimetype='application/json')
-    json = simplejson.dumps({'accept_num': animal.accept_num,
-                                    'name': animal.name,
-                                    'sex': animal.sex,
-                                    'type': animal.type,
-                                    'build': animal.build,
-                                    'age': animal.age,
-                                    'variety': animal.variety,
-                                    'reason': animal.reason,
-                                    'chip_num': animal.chip_num,
-                                    'is_sterilization': animal.is_sterilization,
-                                    'hair_type': animal.hair_type,
-                                    'note': animal.note,
-                                    'resettlement': animal.resettlement,
-                                    'phone': animal.phone,
-                                    'email': animal.email,
-                                    'childre_anlong': animal.childre_anlong,
-                                    'animal_anlong': animal.animal_anlong,
-                                    'bodyweight': animal.bodyweight,
-                                    'image_name': animal.image_name,
-                                    'image_file': animal.image_file,
-                                    'pub_date': animal.pub_date.strftime('%B %d, %Y') })
-    return HttpResponse(json.decode('unicode_escape'), mimetype='application/json')
+    api_json = simplejson.dumps({'accept_num': animal.accept_num,
+                                 'name': animal.name,
+                                 'sex': animal.sex,
+                                 'type': animal.type,
+                                 'build': animal.build,
+                                 'age': animal.age,
+                                 'variety': animal.variety,
+                                 'reason': animal.reason,
+                                 'chip_num': animal.chip_num,
+                                 'is_sterilization': animal.is_sterilization,
+                                 'hair_type': animal.hair_type,
+                                 'note': animal.note,
+                                 'resettlement': animal.resettlement,
+                                 'phone': animal.phone,
+                                 'email': animal.email,
+                                 'childre_anlong': animal.childre_anlong,
+                                 'animal_anlong': animal.animal_anlong,
+                                 'bodyweight': animal.bodyweight,
+                                 'image_name': animal.image_name,
+                                 'image_file': animal.image_file,
+                                 'pub_date': animal.pub_date.strftime('%B %d, %Y')})
+    return HttpResponse(api_json.decode('unicode_escape'), mimetype='application/json')
+
 
 def __get_access_token(request):
     url = "https://graph.facebook.com/oauth/access_token"
@@ -116,8 +117,9 @@ def __get_access_token(request):
     response = urllib2.urlopen(req)
     html = response.read()
     res = html.split("&")
-    access_token = res[0].replace("access_token=","")
+    access_token = res[0].replace("access_token=", "")
     return access_token
+
 
 def __get_app_token(request):
     url = "https://graph.facebook.com/oauth/access_token"
@@ -129,8 +131,9 @@ def __get_app_token(request):
     req = urllib2.Request(url, data)
     response = urllib2.urlopen(req)
     html = response.read()
-    app_token = html.replace("access_token=","")
+    app_token = html.replace("access_token=", "")
     return app_token
+
 
 def __get_debug_json(request, access_token, app_token):
     url = "https://graph.facebook.com/debug_token"
@@ -138,9 +141,10 @@ def __get_debug_json(request, access_token, app_token):
     data['input_token'] = access_token
     data['access_token'] = app_token
     data = urllib.urlencode(data)
-    req = "%s?%s" % (url , data)
+    req = "%s?%s" % (url, data)
     response = urllib2.urlopen(req)
     return response.read()
+
 
 def facebook_login(request):
     access_token = __get_access_token(request)
@@ -159,9 +163,10 @@ def facebook_login(request):
     #TODO:@jslee:check token and update last_login_date
     return HttpResponse(debug_json)
 
+
 def register(request):
-    return render_to_response('register.html',
-            context_instance=RequestContext(request))
+    return render_to_response('register.html', context_instance=RequestContext(request))
+
 
 #TODO@jslee: use view get image
 def get_img(request):
