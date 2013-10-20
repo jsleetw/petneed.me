84b5c462fcda92f060142402b793fce288cff431
thewayiam <twly.tw@gmail.com>
2013-10-20 22:58:53 +0800

    Issue #27: google custom search

--- pets/pets/settings.py
+++ pets/pets/settings.py
@@ -175,6 +175,20 @@ LOGIN_REDIRECT_URL = '/'
 FACEBOOK_EXTENDED_PERMISSIONS = ['email', 'publish_actions', 'user_about_me', 'user_photos', 'user_location']
 SITE_DOMAIN = "petneed.me"
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.contrib.messages.context_processors.messages",
+    "django.core.context_processors.request",
+    'social_auth.context_processors.social_auth_by_name_backends',
+    'social_auth.context_processors.social_auth_backends',
+    'social_auth.context_processors.social_auth_by_type_backends',
+    'social_auth.context_processors.social_auth_login_redirect',    
+)
+
 from local_settings import *
 
 
